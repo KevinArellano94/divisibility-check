9db750b8ecaaec1a208c0daeb837cdf24749edee
Kevin Arellano <Kevin.Arellano94@gmail.com>
2022-04-01 21:05:38 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+def main():
+    print(devisibility_check(9))
+
+def devisibility_check(z):
+    if (z % 5) == 0:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    main()
